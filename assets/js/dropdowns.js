17711a98ee32c61e278f419aa42410a86734735c
Emma <emma1312@protonmail.ch>
2019-03-27 22:18:05 +0100

    redesign site nav, make it no-js accessible

--- assets/js/dropdowns.js
+++ assets/js/dropdowns.js
@@ -10,23 +10,22 @@ const KEY_DOWN = 40;
 
 const FOCUSABLE_ELEMENTS = `> .dropdown__toggle,
 > .dropdown__menu a[href],
-> .dropdown__menu button:not([disabled])
+> .dropdown__menu button:not([disabled]),
 > .dropdown__menu input:not([disabled])`;
 
+const MENU_ACTIONS = `.dropdown__menu a[href],
+.dropdown__menu input[type="button"],
+.dropdown__menu button[type="submit"],
+.dropdown__menu button:not([type])`;
+
 function toggle($dropdown) {
-    // close all other dropdowns
-    toggleAttributes($('.dropdown--expanded').not($dropdown));
+    const $toClose = $dropdown.add($dropdown.parents('.dropdown--expanded'));
+
+    // close all open dropdowns that are not self or a parent of self
+    toggleAttributes($('.dropdown--expanded').not($toClose));
 
     // toggle the current dropdown
     toggleAttributes($dropdown);
-
-    $(document).off('keydown', globalKeyDownHandler);
-    $(document).off('click', globalHtmlClickHandler);
-
-    if ($dropdown.hasClass('dropdown--expanded')) {
-        $(document).on('keydown', globalKeyDownHandler);
-        $(document).one('click', globalHtmlClickHandler);
-    }
 }
 
 function toggleAttributes($dropdowns) {
@@ -62,11 +61,12 @@ function globalKeyDownHandler(event) {
 
     const $dropdown = $('.dropdown--expanded');
 
+    if ($dropdown.length == 0) {
+        return;
+    }
+
     switch (event.which) {
     case KEY_ESC:
-        event.originalEvent.preventDefault();
-        event.originalEvent.stopPropagation();
-
         toggle($dropdown);
 
         // give focus back to toggle
@@ -74,33 +74,33 @@ function globalKeyDownHandler(event) {
 
         break;
     case KEY_DOWN:
-        event.originalEvent.preventDefault();
-        event.originalEvent.stopPropagation();
         moveInList($dropdown, 1);
 
         break;
     case KEY_UP:
-        event.originalEvent.preventDefault();
-        event.originalEvent.stopPropagation();
         moveInList($dropdown, -1);
 
         break;
     case KEY_HOME:
-        event.originalEvent.preventDefault();
-        event.originalEvent.stopPropagation();
         moveInList($dropdown, Infinity);
 
         break;
     case KEY_END:
-        event.originalEvent.preventDefault();
-        event.originalEvent.stopPropagation();
         moveInList($dropdown, -Infinity);
 
         break;
+    default:
+        return;
     }
+
+    event.preventDefault();
 }
 
-function globalHtmlClickHandler() {
+function toggleHandler() {
+    toggle($(this).parent('.dropdown'));
+}
+
+function menuActionsHandler() {
     toggle($('.dropdown--expanded'));
 }
 
@@ -109,10 +109,6 @@ function globalHtmlClickHandler() {
 $('.dropdown').addClass('dropdown-container').addClass('js'); // BC
 $('.dropdown__toggle').attr('aria-haspopup', true).attr('aria-expanded', false);
 
-$(document).on('click', '.dropdown__toggle', function (event) {
-    event.stopPropagation();
-
-    toggle($(this).parent('.dropdown'));
-
-    return false;
-});
+$(document).on('click', '.dropdown__toggle', toggleHandler);
+$(document).on('click', MENU_ACTIONS, menuActionsHandler);
+$(document).on('keydown', globalKeyDownHandler);
